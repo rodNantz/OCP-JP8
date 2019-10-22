c9727dc55445574f7a9c61a465c8e9553a431be9
jxdojocommon <jxdojocommon@gmail.com>
2019-10-21 23:46:15 -0300

    Assessment pg

---
+++ OCP-JP8/src/ocp/assessment/Assessment.java
@@ -0,0 +1,16 @@
+package ocp.assessment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Assessment {
+
+	@Test
+	void test01StreamCyclicBarrier() {
+		
+		
+		
+	}
+
+}
