32eef8e974a78aba07e89f6ebfb1a1d27145bb3c
rodtwo <rodtwo@hotmail.com>
2019-11-28 19:27:52 -0200

    assert demo

--- OCP-JP8/src/ocp/assessment/classes/AssertDemo.java
+++ OCP-JP8/src/ocp/assessment/classes/AssertDemo.java
@@ -2,4 +2,14 @@ package ocp.assessment.classes;
 
 public class AssertDemo {
 	
+	// compiles due to autoboxing
+	public static void main(String [] args) {
+		Integer x = 10;
+		x++;
+		/* the command line desn't throw assertion errors - so nothing happens. 
+		   you must activate -ea to enable it. */ 
+		assert x == null && x >= 0;
+		System.out.println(x);
+	}
+	
 }
